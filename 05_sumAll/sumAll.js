87c8728843919c7dbd584b8cb1e1062025750864
Adith <adithdevakonda@gmail.com>
2022-08-06 12:30:18 -0400

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    sum = 0;
+    if (second <= first) {
+        let temp = 0;
+        temp = first;
+        first = second;
+        second = temp;
+    }
+    for (let i = first; i <= second; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
