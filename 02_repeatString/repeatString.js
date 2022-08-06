2884784b97fa68e8bdedbe9e9555b40484265daa
Adith <adithdevakonda@gmail.com>
2022-08-06 11:08:02 -0400

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, value) {
+    finalWord = "";
+    for (let i = 0; i < value; i++) {
+        finalWord += word;
+    }
+    return finalWord;
 };
 
 // Do not edit below this line
