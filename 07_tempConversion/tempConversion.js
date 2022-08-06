143d18327ca83bd904d6ab64e0a868cba582c42e
Adith <adithdevakonda@gmail.com>
2022-08-06 12:46:17 -0400

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (5/9) * (f - 32);
+  return Number(c.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = ((5/9) * c) + 32;
+  return Number(f.toFixed(1));
 };
 
 // Do not edit below this line
